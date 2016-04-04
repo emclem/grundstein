9242977a6b22faf2813af985b242876f6c9606f7
emclem <ed_m_clemente@yahoo.com>
2016-04-05 02:16:47 +0800

    new template

---
+++ js/main.js
@@ -0,0 +1,20 @@
+jQuery(function($) {'use strict',
+
+	
+	// all Parallax Section
+	$(window).load(function(){'use strict',
+		$("#services").parallax("50%", 0.3);
+		$("#clients").parallax("50%", 0.3);
+	});
+	
+
+});
+
+
+// Slider Height
+var slideHeight = $(window).height();
+$('#slider .carousel-inner .item, #slider .video-container').css('height',slideHeight);
+
+$(window).resize(function(){'use strict',
+	$('#slider .carousel-inner .item, #slider .video-container').css('height',slideHeight);
+});x
