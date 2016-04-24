b29a037027e69f6909e9aede2bacbb947f45cd19
emclem <ed_m_clemente@yahoo.com>
2016-04-24 22:45:21 +0800

    update

--- js/custom.js
+++ js/custom.js
@@ -16,11 +16,33 @@ $(document).ready(function() {
 	});
 
 
+	// SLIDER HEIGHT
+	var slideHeight = $(window).height();
+	$('#slider .carousel-inner .item, #slider .video-container').css('height',slideHeight);
+	$(window).resize(function(){'use strict',
+		$('#slider .carousel-inner .item, #slider .video-container').css('height',slideHeight);
+	});
+
+
+	// BANNER HEIGHT
+	var bannerHeight = $(window).height()/1.75;
+	$('#banner .banner-item').css('height',bannerHeight);
+	$(window).resize(function(){'use strict',
+		$('#banner .banner-item').css('height',bannerHeight);
+	});
+
+
 	// BACK TO TOP
 	$('#totop').click(function() {
 		$('html, body').animate({'scrollTop': '0px'}, 1000);
 		return false;
 	});
+
+
+	// PRETTY PHOTO
+	$("a[rel^='prettyPhoto']").prettyPhoto({
+		social_tools: false
+	});	
 
 
 });
